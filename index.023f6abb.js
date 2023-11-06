e9814ee3b2ac3594bd41f57b383a3b7545990a8a
SVTroya <my.svtroy@gmail.com>
2023-11-06 12:13:18 +0000

    Deploying to gh-pages from @ SVTroya/goit-js-hw-10@ab90298e4fc932bc6eb070fed491ed6e1c56692c 🚀

---
+++ index.023f6abb.js
@@ -0,0 +1,2 @@
+document.querySelector(".main-choice").addEventListener("click",(function(e){var t=e.target;"A"===t.nodeName&&t.dataset.petType}));
+//# sourceMappingURL=index.023f6abb.js.map
---
+++ index.45fcde6e.js
@@ -0,0 +1,2 @@
+document.querySelector(".main-choice").addEventListener("click",(function({target:e}){if("A"!==e.nodeName)return;e.dataset.petType}));
+//# sourceMappingURL=index.45fcde6e.js.map
